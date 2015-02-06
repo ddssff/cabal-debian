d9f985a8b4502835f35df112c0bbaf629e1f27ce
David Fox <dsf@seereason.com>
2015-02-06 05:26:20 -0800

    Update the debian/Debianize.hs script so it matches the checked in debianization

--- debian/Debianize.hs
+++ debian/Debianize.hs
@@ -13,6 +13,7 @@ import Control.Monad.State (get)
 import Data.Default (def)
 import Data.Lens.Lazy (getL, access)
 import Data.List (intercalate)
+import Data.Map as Map (insert)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mempty)
 import Data.Set as Set (singleton, insert)
@@ -69,61 +70,18 @@ main =
              (standardsVersion . control . debInfo) ~= Just (StandardsVersion 3 9 3 Nothing)
              (compat . debInfo) ~= Just 9
              (utilsPackageNameBase . debInfo) ~= Just "cabal-debian"
-             (copyright . debInfo) %= (Just . copyrightFn . fromMaybe def)
-             (conflicts . relations . binaryDebDescription (BinPkgName "cabal-debian") . debInfo) %= (++ (rels "haskell-debian-utils (<< 3.59)"))
+             -- (copyright . debInfo) %= (Just . copyrightFn . fromMaybe def)
+             -- (conflicts . relations . binaryDebDescription (BinPkgName "cabal-debian") . debInfo) %= (++ (rels "haskell-debian-utils (<< 3.59)"))
              (depends . relations . binaryDebDescription (BinPkgName "cabal-debian") . debInfo) %= (++ (rels "apt-file, debian-policy, debhelper, haskell-devscripts (>= 0.8.19)"))
              (depends . relations . binaryDebDescription (BinPkgName "libghc-cabal-debian-dev") . debInfo) %= (++ (rels "debian-policy"))
-             (atomSet . debInfo) %= (Set.insert $ InstallCabalExec (BinPkgName "cabal-debian-tests") "cabal-debian-tests" "/usr/bin")
-             (atomSet . debInfo) %= (Set.insert $ InstallCabalExec (BinPkgName "cabal-debian") "cabal-debian" "/usr/bin")
+             (executable . debInfo) %= (Map.insert (BinPkgName "cabal-debian-tests") (InstallFile "cabal-debian-tests" Nothing Nothing "cabal-debian-tests"))
+             (atomSet . debInfo) %= (Set.insert $ InstallCabalExec (BinPkgName "cabal-debian") "cabal-debian" "usr/bin")
              (utilsPackageNameBase . debInfo) ~= Just "cabal-debian"
              -- extraDevDeps (BinPkgName "debian-policy")
              (homepage . control . debInfo) ~= Just (pack "https://github.com/ddssff/cabal-debian")
 
 rels :: String -> Relations
 rels = either (throw . userError . show) id . parseRelations
-
--- | Demonstrates the structure of the new copyright type.
-copyrightFn :: CopyrightDescription -> CopyrightDescription
-copyrightFn =
-    const $ def     { _filesAndLicenses =       [FilesDescription { _filesPattern = "*"
-                                                                  , _filesCopyright = pack (unlines [ "Copyright (c) 2007, David Fox"
-                                                                                                    , "Copyright (c) 2007, Jeremy Shaw" ])
-                                                                  , _filesLicense = OtherLicense "Proprietary"
-                                                                  , _filesComment = Just $ pack $ unlines
-                                                                                    [ "All rights reserved."
-                                                                                    , ""
-                                                                                    , "The packageing was adjusted to Debian conventions by Joachim Breitner"
-                                                                                    , "<nomeata@debian.org> on Sat, 01 May 2010 21:16:18 +0200, and is licenced under"
-                                                                                    , "the same terms as the package itself.."
-                                                                                    , ""
-                                                                                    , "Redistribution and use in source and binary forms, with or without"
-                                                                                    , "modification, are permitted provided that the following conditions are"
-                                                                                    , "met:"
-                                                                                    , ""
-                                                                                    , "    * Redistributions of source code must retain the above copyright"
-                                                                                    , "      notice, this list of conditions and the following disclaimer."
-                                                                                    , ""
-                                                                                    , "    * Redistributions in binary form must reproduce the above"
-                                                                                    , "      copyright notice, this list of conditions and the following"
-                                                                                    , "      disclaimer in the documentation and/or other materials provided"
-                                                                                    , "      with the distribution."
-                                                                                    , ""
-                                                                                    , "    * The names of contributors may not be used to endorse or promote"
-                                                                                    , "      products derived from this software without specific prior"
-                                                                                    , "      written permission."
-                                                                                    , ""
-                                                                                    , "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS"
-                                                                                    , "\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT"
-                                                                                    , "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR"
-                                                                                    , "A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT"
-                                                                                    , "OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,"
-                                                                                    , "SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT"
-                                                                                    , "LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,"
-                                                                                    , "DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY"
-                                                                                    , "THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT"
-                                                                                    , "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE"
-                                                                                    , "OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." ] }]
-                    , _summaryComment = Just $ pack "This package is not part of the Debian GNU/Linux distribution." }
 
 -- | This copies the first log entry of deb1 into deb2.  Because the
 -- debianization process updates that log entry, we need to undo that

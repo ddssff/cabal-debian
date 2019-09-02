917770e638331307ac17cc21819b97bd7bccc218
Clint Adams <clint@debian.org>
2019-09-01 22:09:32 -0400

    Special-case haskell-src-exts-simple

--- src/Debian/Debianize/Details.hs
+++ src/Debian/Debianize/Details.hs
@@ -29,13 +29,8 @@ import Distribution.Package (PackageName(PackageName))
 debianDefaults :: Monad m => CabalT m ()
 debianDefaults =
     do -- These are the two epoch names I know about in the debian repo
-#if MIN_VERSION_Cabal(2,0,0)
        A.epochMap %= Map.insert (mkPackageName "HaXml") 1
        A.epochMap %= Map.insert (mkPackageName "HTTP") 1
-#else
-       A.epochMap %= Map.insert (PackageName "HaXml") 1
-       A.epochMap %= Map.insert (PackageName "HTTP") 1
-#endif
        -- Associate some build tools and their corresponding
        -- (eponymous) debian package names
        mapM_ (\name -> (A.debInfo . D.execMap) %= Map.insert name [[Rel (BinPkgName name) Nothing Nothing]])
@@ -43,40 +38,20 @@ debianDefaults =
        -- The parsec debs are suffixed with either "2" or "3"
 #if !MIN_VERSION_base(4,11,1)
 -- The deb of ghc-8.4.3 changes the libghc-parsec3-dev name to libghc-parsec-deb
-#if MIN_VERSION_Cabal(2,0,0)
        mapCabal (mkPackageName "parsec") (DebBase "parsec3")
-#else
-       mapCabal (PackageName "parsec") (DebBase "parsec3")
-#endif
 #endif
        -- Similar split for quickcheck
-#if MIN_VERSION_Cabal(2,0,0)
        mapCabal (mkPackageName "QuickCheck") (DebBase "quickcheck2")
-#else
-       mapCabal (PackageName "QuickCheck") (DebBase "quickcheck2")
-#endif
        -- Something was required for this package at one time - it
        -- looks like a no-op now
-#if MIN_VERSION_Cabal(2,0,0)
        mapCabal (mkPackageName "gtk2hs-buildtools") (DebBase "gtk2hs-buildtools")
-#else
-       mapCabal (PackageName "gtk2hs-buildtools") (DebBase "gtk2hs-buildtools")
-#endif
        -- Upgrade transformers to 0.4 - no don't!
        -- remapCabal (PackageName "transformers") (DebBase "transformers4")
        -- remapCabal (PackageName "haskeline") (DebBase "haskeline0713")
-#if MIN_VERSION_Cabal(2,0,0)
        mapCabal (mkPackageName "haskell-src-exts") (DebBase "src-exts")
+       mapCabal (mkPackageName "haskell-src-exts-simple") (DebBase "src-exts-simple")
        mapCabal (mkPackageName "haskell-src-meta") (DebBase "src-meta")
        mapCabal (mkPackageName "Cabal") (DebBase "cabal")
 
        mapCabal (mkPackageName "happstack-authenticate") (DebBase "happstack-authenticate")
        splitCabal (mkPackageName "happstack-authenticate") (DebBase "happstack-authenticate-0") (mkVersion [2])
-#else
-       mapCabal (PackageName "haskell-src-exts") (DebBase "src-exts")
-       mapCabal (PackageName "haskell-src-meta") (DebBase "src-meta")
-       mapCabal (PackageName "Cabal") (DebBase "cabal")
-
-       mapCabal (PackageName "happstack-authenticate") (DebBase "happstack-authenticate")
-       splitCabal (PackageName "happstack-authenticate") (DebBase "happstack-authenticate-0") (Version [2] [])
-#endif
